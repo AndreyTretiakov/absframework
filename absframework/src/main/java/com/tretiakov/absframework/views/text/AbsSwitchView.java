832a09398e9b694ba30bc158df36ce85ad1f277f
Andrew Tretiakov <andrewtretiakov@Andrews-MacBook-Pro.local>
2017-02-19 19:44:33 +0200

    engine

---
+++ absframework/src/main/java/com/tretiakov/absframework/views/text/AbsSwitchView.java
@@ -0,0 +1,36 @@
+package com.tretiakov.absframework.views.text;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.SwitchCompat;
+import android.util.AttributeSet;
+
+import com.tretiakov.absframework.R;
+
+/**
+ * @author Andrey Tretiakov. Created 4/15/2016.
+ */
+public class AbsSwitchView extends SwitchCompat {
+
+    public AbsSwitchView(@NonNull Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public AbsSwitchView(@NonNull Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(@NonNull Context context, AttributeSet attrs) {
+        if (!isInEditMode()) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AbsFont);
+            String font = a.getString(R.styleable.AbsFont_font);
+            a.recycle();
+
+            setTypeface(FontsHelper.getTypeFace(getContext(), "fonts/" +
+                    (font == null ? "Roboto-Regular" : font) + ".ttf"));
+        }
+    }
+}
