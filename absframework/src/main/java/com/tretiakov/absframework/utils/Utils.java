946ba6810ca3f9c705d8e2d25fd9cb0ded6c9b2e
Andrew Tretiakov <andrewtretiakov@Andrews-MacBook-Pro.local>
2017-03-21 16:13:11 +0200

    developing

---
+++ absframework/src/main/java/com/tretiakov/absframework/utils/Utils.java
@@ -0,0 +1,15 @@
+package com.tretiakov.absframework.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author andrewtretiakov; 3/21/17.
+ */
+
+public class Utils {
+
+    public static List emptyList() {
+        return new ArrayList();
+    }
+}
