e59e6dc2a3eac9e6dc4578565081046918a320cb
Andrew Tretiakov <andrewtretiakov@Andrews-MacBook-Pro.local>
2017-04-10 13:54:26 +0300

    developing

--- absframework/src/main/java/com/tretiakov/absframework/utils/Utils.java
+++ absframework/src/main/java/com/tretiakov/absframework/utils/Utils.java
@@ -1,7 +1,12 @@
 package com.tretiakov.absframework.utils;
 
+import android.content.Context;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * @author andrewtretiakov; 3/21/17.
@@ -15,5 +20,25 @@ public class Utils {
 
     public static Object[] toArray(Object... objects) {
         return objects;
+    }
+
+    public static String userAgent_(Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int densityDpi = (int) (metrics.density * 160f);
+        int displayHeight = metrics.heightPixels;
+        int displayWidth = metrics.widthPixels;
+
+        return String.format("Android (%s/%s; %s; %s; %s; %s; %s; %s; %s_%s)",
+                Build.VERSION.RELEASE,
+                Build.VERSION.SDK,
+                densityDpi + "dpi",
+                displayHeight + "x" + displayWidth,
+                Build.MODEL,
+                Build.MANUFACTURER,
+                Build.BRAND,
+                Build.DEVICE,
+                Locale.getDefault().getLanguage(),
+                Locale.getDefault().getCountry()
+        );
     }
 }
