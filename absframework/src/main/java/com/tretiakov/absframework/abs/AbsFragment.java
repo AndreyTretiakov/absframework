765420187fa20d301d8c2fd7de146cd95ec6ff9a
Andrew Tretiakov <at.box.ua@gmail.com>
2017-09-12 00:43:02 +0300

    developing

--- absframework/src/main/java/com/tretiakov/absframework/abs/AbsFragment.java
+++ absframework/src/main/java/com/tretiakov/absframework/abs/AbsFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
