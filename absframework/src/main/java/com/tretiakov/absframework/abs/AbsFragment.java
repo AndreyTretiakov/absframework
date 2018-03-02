a2ab8d44659ef77b586d442c2f4add26030a944e
Andrew Tretiakov <at.box.ua@gmail.com>
2018-03-02 21:19:05 +0200

    RC 1.13.G (actual following)

--- absframework/src/main/java/com/tretiakov/absframework/abs/AbsFragment.java
+++ absframework/src/main/java/com/tretiakov/absframework/abs/AbsFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
